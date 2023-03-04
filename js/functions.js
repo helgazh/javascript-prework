4417803ec84c48219a70bd47fd5b5c5d1104d454
HelZh <helgazhadan@gmail.com>
2023-03-04 15:56:44 +0100

    Add buttons

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,62 @@
 function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
+	let div1 = document.createElement('div');
+	div1.innerHTML = msg;
 
-    document.getElementById('messages').appendChild(div);
+    document.getElementById('messages').appendChild(div1);
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+function getMoveName(number){
+	if(number == 1){
+		return 'rock';
+	} else if (number == 2){
+		return 'scissors';
+	} else if (number == 3){
+		return 'paper';
+	} else {
+		printMessage('Error');
+	}
+	}
+
+	function displayResult(argComputerMove,argPlayerMove){
+		if (argComputerMove == 'rock' && argPlayerMove == 'paper'){
+			printMessage('You win!');
+		  } else if (argComputerMove == 'rock' && argPlayerMove == 'scissors'){
+			printMessage('You lose!');
+		  } else if (argComputerMove == 'rock' && argPlayerMove == 'rock'){
+			printMessage('Draw!');
+		  } else if (argComputerMove == 'scissors' && argPlayerMove == 'paper'){
+			printMessage('You loser!');
+		  }  else if (argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
+			printMessage('Draw!');
+		}  else if (argComputerMove == 'scissors' && argPlayerMove == 'rock'){
+			printMessage('You win!');
+		  }  else if (argComputerMove == 'paper' && argPlayerMove == 'scissors'){
+			printMessage('You win!');
+		  }  else if (argComputerMove == 'paper' && argPlayerMove == 'paper'){
+			printMessage('Draw!');
+		}  else if (argComputerMove == 'paper' && argPlayerMove == 'rock'){
+			printMessage('You lose!');
+		  } else{
+			printMessage('Error! Try again later');
+		  }
+		}
+	
+		function playGame(playerInput){
+			clearMessages();
+
+			let randomFraction = Math.random();
+			let calculation = randomFraction * 3 + 1;
+			let roundNumber = Math.floor(calculation);
+			let computerMove = getMoveName(roundNumber);
+			let playerMove = getMoveName(playerInput);
+
+			printMessage('I choose for my course: ' + computerMove);
+			printMessage('Twój ruch to: ' + playerMove);
+			displayResult(computerMove,playerMove);
+
+
+		}
--- js/script.js
+++ js/script.js
@@ -1,53 +1,15 @@
-function getMoveName(number){
-if(number == 1){
-    return 'rock';
-} else if (number == 2){
-    return 'scissors';
-} else if (number == 3){
-    return 'paper';
-} else {
-    printMessage('Error');
-}
-}
+// playGame(2);
 
-let randomFraction = Math.random();
+    let rock = document.getElementById('rock');
+    let scissors = document.getElementById('scissors');
+    let paper = document.getElementById('paper');
 
-let calculation = randomFraction * 3 + 1;
-
-let roundNumber = Math.floor(calculation);
-
-let computerMove = getMoveName(roundNumber);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-let playerMove = getMoveName(playerInput);
-
-function displayResult(argComputerMove,argPlayerMove){
-    if (argComputerMove == 'rock' && argPlayerMove == 'paper'){
-        printMessage('You win!');
-      } else if (argComputerMove == 'rock' && argPlayerMove == 'scissors'){
-        printMessage('You lose!');
-      } else if (argComputerMove == 'rock' && argPlayerMove == 'rock'){
-        printMessage('Draw!');
-      } else if (argComputerMove == 'scissors' && argPlayerMove == 'paper'){
-        printMessage('You loser!');
-      }  else if (argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
-        printMessage('Draw!');
-    }  else if (argComputerMove == 'scissors' && argPlayerMove == 'rock'){
-        printMessage('You win!');
-      }  else if (argComputerMove == 'paper' && argPlayerMove == 'scissors'){
-        printMessage('You win!');
-      }  else if (argComputerMove == 'paper' && argPlayerMove == 'paper'){
-        printMessage('Draw!');
-    }  else if (argComputerMove == 'paper' && argPlayerMove == 'rock'){
-        printMessage('You lose!');
-      } else{
-        printMessage('Error! Try again later');
-      }
-    }
-
-printMessage('I choose for my course: ' + computerMove);
-
-printMessage('Twój ruch to: ' + playerMove);
-
-displayResult(computerMove,playerMove);
+    rock.addEventListener('click', function(){
+        playGame(1);
+    });
+    scissors.addEventListener('click', function(){
+        playGame(2);
+    });
+    paper.addEventListener('click', function(){
+        playGame(3);
+    });
